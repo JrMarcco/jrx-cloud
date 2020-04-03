55b31e0e6d739fc8a888149b1dca98116c041133
hongjinchai <hongjinchai@nxin.com>
2020-04-03 10:52:14 +0800

    commit

--- jrx-cloud-common/src/main/java/com/jrx/cloud/common/util/ShellExecUtils.java
+++ jrx-cloud-common/src/main/java/com/jrx/cloud/common/util/ShellExecUtils.java
@@ -7,7 +7,6 @@ import org.springframework.util.CollectionUtils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
