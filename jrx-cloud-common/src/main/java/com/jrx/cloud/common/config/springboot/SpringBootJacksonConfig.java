92b188723c8d9a38aea105720e795156a924acfb
hongjinchai <hongjinchai@nxin.com>
2020-10-15 11:49:13 +0800

    commit

---
+++ jrx-cloud-common/src/main/java/com/jrx/cloud/common/config/springboot/SpringBootJacksonConfig.java
@@ -0,0 +1,24 @@
+package com.jrx.cloud.common.config.springboot;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author jrmarcco
+ * @version 1.0  2020/10/15
+ */
+@Configuration
+public class SpringBootJacksonConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+
+        return jacksonObjectMapperBuilder -> {
+            jacksonObjectMapperBuilder.serializerByType(Long.TYPE, ToStringSerializer.instance);
+            jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
+        };
+
+    }
+}
