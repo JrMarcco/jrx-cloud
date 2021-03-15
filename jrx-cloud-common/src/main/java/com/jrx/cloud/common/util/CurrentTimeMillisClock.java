c53172e0e4416a1024285833c161b6ff2822df6f
hongjinchai <hongjinchai@nxin.com>
2021-03-15 09:54:13 +0800

    commit

---
+++ jrx-cloud-common/src/main/java/com/jrx/cloud/common/util/CurrentTimeMillisClock.java
@@ -0,0 +1,38 @@
+package com.jrx.cloud.common.util;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author hongjc
+ * @version 1.0  2021/3/15
+ */
+public class CurrentTimeMillisClock {
+
+    private volatile long now;
+
+    private CurrentTimeMillisClock() {
+        this.now = System.currentTimeMillis();
+        scheduleTick();
+    }
+
+    private void scheduleTick() {
+        new ScheduledThreadPoolExecutor(1, runnable -> {
+            Thread thread = new Thread(runnable, "current-time-millis");
+            thread.setDaemon(true);
+            return thread;
+        }).scheduleAtFixedRate(() -> now = System.currentTimeMillis(), 1, 1, TimeUnit.MILLISECONDS);
+    }
+
+    public long now() {
+        return now;
+    }
+
+    public static CurrentTimeMillisClock getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final CurrentTimeMillisClock INSTANCE = new CurrentTimeMillisClock();
+    }
+}
